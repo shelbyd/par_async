4bc19e8aeab390f55dc5233593f61708c7427762
Shelby Doolittle <shelby@shelbyd.com>
2019-09-19 23:11:08 -0400

    panic if future moved before drop

--- tests/support/echo.rs
+++ tests/support/echo.rs
@@ -1,5 +1,6 @@
 use crate::support::pollable::*;
 use core::future::Future;
+use futures_test::task::noop_context;
 use std::collections::{HashMap, HashSet};
 use std::{
     cell::RefCell,
@@ -41,6 +42,8 @@ impl Echo {
             shared: self.shared.clone(),
             input: s.to_string(),
             returned: false,
+            _pinned: core::marker::PhantomPinned,
+            first_poll_location: None,
         }
     }
 
@@ -62,26 +65,51 @@ pub struct EchoFuture {
     shared: Rc<RefCell<SharedEcho>>,
     input: String,
     returned: bool,
+    _pinned: core::marker::PhantomPinned,
+    first_poll_location: Option<*const EchoFuture>,
 }
 
 impl Future for EchoFuture {
     type Output = String;
-    fn poll(mut self: Pin<&mut Self>, _: &mut Context) -> Poll<Self::Output> {
+    fn poll(self: Pin<&mut Self>, _: &mut Context) -> Poll<Self::Output> {
         if self.returned {
             panic!("Polled previously resolved future");
         }
 
-        let borrow = || self.shared.borrow_mut();
+        let this = unsafe { self.get_unchecked_mut() };
 
-        *borrow().polls.entry(self.input.clone()).or_insert(0) += 1;
+        let im_here = this as *const EchoFuture;
+        match this.first_poll_location {
+            Some(ptr) if im_here != ptr => panic!("Future moved between polls"),
+            None => this.first_poll_location = Some(im_here),
+            _ => {}
+        }
 
-        if borrow().returns.contains(&self.input) {
-            borrow().returns.remove(&self.input);
-            self.returned = true;
+        let borrow = || this.shared.borrow_mut();
 
-            Poll::Ready(self.input.to_string())
+        *borrow().polls.entry(this.input.clone()).or_insert(0) += 1;
+
+        if borrow().returns.contains(&this.input) {
+            borrow().returns.remove(&this.input);
+            this.returned = true;
+
+            Poll::Ready(this.input.to_string())
         } else {
             Poll::Pending
+        }
+    }
+}
+
+// Extremely sensitive implementation of Drop that panics if the future was moved after poll was
+// called.
+impl Drop for EchoFuture {
+    fn drop(&mut self) {
+        if let Some(ptr) = self.first_poll_location {
+            if ptr != self {
+                if !::std::thread::panicking() {
+                    panic!("Future moved before drop");
+                }
+            }
         }
     }
 }
@@ -143,5 +171,34 @@ mod echo_tests {
         let _ = p.poll();
 
         let _ = p.poll(); // Panic here.
+    }
+
+    // Moving a pinned value violates the contract of Pin. This test ensures that our code would
+    // panic if that contract is violated.
+    #[test]
+    #[should_panic(expected = "Future moved between polls")]
+    fn echo_move_after_poll_panics() {
+        let e = Echo::new();
+        let mut fut = e.echo("foo");
+
+        let mut cx = noop_context();
+        let _ = unsafe { Pin::new_unchecked(&mut fut) }.poll(&mut cx);
+
+        let mut moved_fut = fut;
+        let _ = unsafe { Pin::new_unchecked(&mut moved_fut) }.poll(&mut cx); // Panic here.
+    }
+
+    // Moving a pinned value before it is dropped violates the contract of Pin. This test ensures
+    // that our code would panic if that contract is violated.
+    #[test]
+    #[should_panic(expected = "Future moved before drop")]
+    fn echo_move_after_poll_before_drop() {
+        let e = Echo::new();
+        let mut fut = e.echo("foo");
+
+        let mut cx = noop_context();
+        let _ = unsafe { Pin::new_unchecked(&mut fut) }.poll(&mut cx);
+
+        let mut _moved_fut = fut;
     }
 }
